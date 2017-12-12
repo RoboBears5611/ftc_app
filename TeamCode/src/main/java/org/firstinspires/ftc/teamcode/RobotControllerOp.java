8e37a999d3df3755399f71f7efb08b445d337bc7
Hermanoid <lucasniewohner@gmail.com>
2017-12-12 16:48:59 -0600

    all together arm, lift and move
    +arm reduce power

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControllerOp.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by FTC on 11/21/2016.
+ */
+
+@TeleOp(name = "Robot Controller (Full)",group = "drive")
+public class RobotControllerOp extends OpMode {
+    MechanumDriveBase mechanumDriveBase;
+    DcMotor Grabber;
+    DcMotor Lift;
+    final static float GrabberSpeed  = 0.1f;
+
+
+    @Override
+    public void init(){
+        mechanumDriveBase = new MechanumDriveBase(hardwareMap,telemetry);
+        mechanumDriveBase.init();
+        Grabber = hardwareMap.dcMotor.get("Grabber");
+        Lift = hardwareMap.dcMotor.get("Lift");
+        Grabber.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+
+    @Override
+    public void start(){
+        mechanumDriveBase.stopAllMotors();
+    }
+
+    @Override
+    public void loop(){
+        mechanumDriveBase.move(gamepad2.right_stick_x,gamepad1.right_stick_y,gamepad1.left_stick_x);
+        Grabber.setPower((gamepad1.a?GrabberSpeed:0)-(gamepad1.b?GrabberSpeed:0));
+        Lift.setPower((gamepad1.x?1:0)-(gamepad1.y?0.25:0));
+    }
+}
