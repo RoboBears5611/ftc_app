691e0d201a41335a6aeaebdaa91e9a3e69618b2c
Hermanoid <lucasniewohner@gmail.com>
2018-02-08 19:42:22 -0600

    New Time Based drive method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTester3000Op.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTester3000Op.java
@@ -5,9 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -16,90 +18,96 @@ import java.util.Set;
 /**
  * Created by FTC on 12/13/2016.
  */
-@TeleOp(group="TEST",name="DriveTester3000")
+@TeleOp(group="TEST",name="DriveTester3000 (OnBot)")
 public class DriveTester3000Op extends OpMode {
-    private final static String LFMotorDefaultName = "LFMotor";
-    private final static String RFMotorDefaultName = "RFMotor";
-    private final static String LBMotorDefaultName = "LBMotor";
-    private final static String RBMotorDefaultName = "RBMotor";
+    private TimeBasedMechanumDriveBase timeBasedMechanumDriveBase;
+    private float x;
+    private float y;
+    private float turn;
+    private int time;
+    private float prevX;
+    private float prevY;
+    private float prevTurn;
+    private int prevTime;
+    private boolean canUndo= false;
+    private ChangableField CurrentlyChanging = ChangableField.none;
 
-    private final static boolean LFMotorDefaultReversed = false;
-    private final static boolean RFMotorDefaultReversed = true;
-    private final static boolean LBMotorDefaultReversed = false;
-    private final static boolean RBMotorDefaultReversed = true;
 
-    private DcMotor LFMotor;
-    private DcMotor RFMotor;
-    private DcMotor LBMotor;
-    private DcMotor RBMotor;
-    public DriveTester3000Op(){
-    }
 
 
     @Override
     public void init() {
-        init(   LFMotorDefaultName, LFMotorDefaultReversed,
-                RFMotorDefaultName, RFMotorDefaultReversed,
-                LBMotorDefaultName, LBMotorDefaultReversed,
-                RBMotorDefaultName, RBMotorDefaultReversed);
+        timeBasedMechanumDriveBase = new TimeBasedMechanumDriveBase(hardwareMap,telemetry);
+
+        timeBasedMechanumDriveBase.init();
     }
 
     @Override
     public void loop() {
-        LFMotor.setPower(gamepad1.left_stick_y);
-        RFMotor.setPower(gamepad1.right_stick_y);
-        LBMotor.setPower(gamepad2.left_stick_y);
-        RBMotor.setPower(gamepad2.right_stick_y);
+        //Change current field set to be modified
+        if(gamepad1.x){
+            CurrentlyChanging = ChangableField.x;
+        }else if(gamepad1.y){
+            CurrentlyChanging = ChangableField.y;
+        }else if(gamepad1.a){
+            CurrentlyChanging = ChangableField.turn;
+        }else if(gamepad1.b){
+            CurrentlyChanging = ChangableField.time;
+        }
+
+        //Change the selected field by an amount specified by the right stick's y, using the right bumper as a speed boost for impatient people.
+        if(CurrentlyChanging==ChangableField.time){
+            time-=(int)gamepad1.right_stick_y*2*(gamepad1.right_bumper?10:1);
+            if(time<0)time=0;
+        }else{
+            float amount = -gamepad1.right_stick_y/500*(gamepad1.right_bumper?5:1);
+            switch(CurrentlyChanging){
+                case x:
+                    x = Range.clip(x+amount,-1,1);
+                    break;
+                case y:
+                    y= Range.clip(y+amount,-1,1);
+                    break;
+                case turn:
+                    turn= Range.clip(turn+amount,-1,1);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //execute set settings
+        if(gamepad1.dpad_up&&(!timeBasedMechanumDriveBase.isBusy())){
+            move(x,y,turn,time);
+            prevX = x;
+            prevY = y;
+            prevTurn = turn;
+            prevTime = time;
+            canUndo = true;
+        }
+
+        //undo
+        if(gamepad1.dpad_down&&!timeBasedMechanumDriveBase.isBusy()&&canUndo){
+            canUndo = false;
+            move(-prevX,-prevY,-prevTurn,prevTime);
+        }
+
+          TimeBasedMechanumDriveBase.TimeBasedMechanumDriveMoveResult result = timeBasedMechanumDriveBase.loop();
+
+        telemetry.addData("X:  ",x);
+        telemetry.addData("Y:  ",y);
+        telemetry.addData("Turn:  ",turn);
+        telemetry.addData("Time:  ",time);
+        telemetry.addData("CurrentlyChanging:  ",CurrentlyChanging);
+        telemetry.addData("Time Until Completion:  ",result.timeRemainingForCurrentAction);
+        telemetry.addData("Currently Executing:  ",result.isBusy);
+        telemetry.addData("Can Undo",canUndo);
     }
-
-    public void init(String LFMotorName, boolean LFMotorReversed,
-                     String RFMotorName, boolean RFMotorReversed,
-                     String LBMotorName, boolean LBMotorReversed,
-                     String RBMotorName, boolean RBMotorReversed) {
-        RFMotor = hardwareMap.dcMotor.get(RFMotorName);
-        RFMotor.setDirection(RFMotorReversed? DcMotorSimple.Direction.REVERSE: DcMotorSimple.Direction.FORWARD);
-        LFMotor = hardwareMap.dcMotor.get(LFMotorName);
-        LFMotor.setDirection(LFMotorReversed? DcMotorSimple.Direction.REVERSE: DcMotorSimple.Direction.FORWARD);
-        RBMotor = hardwareMap.dcMotor.get(RBMotorName);
-        RBMotor.setDirection(RBMotorReversed? DcMotorSimple.Direction.REVERSE: DcMotorSimple.Direction.FORWARD);
-        LBMotor = hardwareMap.dcMotor.get(LBMotorName);
-        LBMotor.setDirection(LBMotorReversed? DcMotorSimple.Direction.REVERSE: DcMotorSimple.Direction.FORWARD);
-        RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
+    private void move(float x, float y, float turn, long time){
+        timeBasedMechanumDriveBase.addInstruction(x,y,turn,time);
+        if(!timeBasedMechanumDriveBase.executeInstructions()) telemetry.addLine("Failed to execute autonomous instructions");
     }
-
-
-    public void stopAllMotors(){
-        LFMotor.setPower(0);
-        RFMotor.setPower(0);
-        LBMotor.setPower(0);
-        RBMotor.setPower(0);
+    private enum ChangableField {
+        none,x,y,turn,time
     }
-
-    public void move(float x, float y, float turn){
-        telemetry.addData("Drive X",x);
-        telemetry.addData("Drive Y",y);
-        telemetry.addData("Drive Turn",turn);
-
-        //good fun.
-        double r = Math.hypot(x, y); //Get how far the joystick is pushed
-        double robotAngle = Math.atan2(y, x) - Math.PI / 4; //Rotate everything 45 degrees, since the wheels operate on tires with an exactly 45 degree offset from forwards and backwards on the robot
-        final double LF = r * Math.cos(robotAngle) + turn; //Calculate just how much speed each wheel should get in relation to each other (as stated by some Trig!) and then multiply that by how much power we actually want (Sin and Cos won't give us that, since they are just working with angles)
-        final double RF = r * Math.sin(robotAngle) - turn; //We then subtract or add to the power, according to the wheels orientation to the rest of the bot.  This change causes the robot to turn.
-        final double LB = r * Math.sin(robotAngle) + turn;
-        final double RB = r * Math.cos(robotAngle) - turn;
-
-        LFMotor.setPower(LF);
-        telemetry.addData("LFMotor",LF);
-        RFMotor.setPower(RF);
-        telemetry.addData("RFMotor",RF);
-        LBMotor.setPower(LB);
-        telemetry.addData("LBMotor",LB);
-        RBMotor.setPower(RB);
-        telemetry.addData("RBMotor",RB);
-    }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeBasedMechanumDriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeBasedMechanumDriveBase.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,33 +12,87 @@ import java.util.TimerTask;
 public class TimeBasedMechanumDriveBase{
     private MechanumDriveBase mechanumDriveBase;
     private boolean actionIsExecuting;
-    private Timer timer;
+    private Runnable runOnComplete;
+    private Telemetry telemetry;
+    public ArrayList<TimeBasedMechanumDriveMovement> instructions;
+    private TimeBasedMechanumDriveMovement currentMovement;
 
 
     public TimeBasedMechanumDriveBase(HardwareMap hardwareMap,Telemetry telemetry){
+        this.telemetry = telemetry;
         mechanumDriveBase = new MechanumDriveBase(hardwareMap,telemetry);
-        timer = new Timer();
-
+        instructions = new ArrayList<>();
+    }
+    public void init(){
+        mechanumDriveBase.init();
     }
     public boolean isBusy(){
         return actionIsExecuting;
     }
 
-    public boolean move(float x,float y,float turn, long timeMillis){
-        if(isBusy()){
+    public void addInstruction(float x,float y, float turn, long timeMillis){
+        instructions.add(new TimeBasedMechanumDriveMovement(x,y,turn,timeMillis));
+    }
+
+    public TimeBasedMechanumDriveMoveResult loop(){
+        if(actionIsExecuting&&currentMovement.stopTime>=new Date().getTime()){
+            currentMovement = null;
+            if(!executeNextInstruction()) {
+                actionIsExecuting = false;
+                return new TimeBasedMechanumDriveMoveResult(false);
+            }
+        }
+        return new TimeBasedMechanumDriveMoveResult(true, currentMovement.stopTime-new Date().getTime(),instructions.size());
+    }
+
+    public boolean executeInstructions(){
+        if(instructions.isEmpty()||actionIsExecuting){
             return false;
         }
-        mechanumDriveBase.move(x,y,turn);
+        executeNextInstruction();
         actionIsExecuting = true;
-        timer.schedule(new EndAction(),timeMillis);
         return true;
-    }
-    private class EndAction extends TimerTask{
 
-        @Override
-        public void run() {
-            mechanumDriveBase.stopAllMotors();
-            actionIsExecuting = false;
+    }
+
+    private boolean executeNextInstruction(){
+        if(currentMovement==null||instructions.isEmpty()) {
+            return false;
+        }
+        currentMovement = instructions.remove(0);
+        currentMovement.stopTime = new Date().getTime() + currentMovement.time;
+        mechanumDriveBase.move(currentMovement.x, currentMovement.y, currentMovement.turn);
+        return true;
+
+    }
+
+
+    public class TimeBasedMechanumDriveMoveResult{
+        public boolean isBusy;
+        public long timeRemainingForCurrentAction = 0;
+//        public long timeRemainingForAllActions = 0;
+        public int actionsRemaining = 0;
+        public TimeBasedMechanumDriveMoveResult(boolean isBusy){
+            this.isBusy = isBusy;
+        }
+        public TimeBasedMechanumDriveMoveResult(boolean isBusy, long timeRemainingForCurrentAction, int actionsRemaining){
+            this.isBusy = isBusy;
+            this.timeRemainingForCurrentAction = timeRemainingForCurrentAction;
+//            this.timeRemainingForAllActions = timeRemainingForAllActions;
+            this.actionsRemaining = actionsRemaining;
+        }
+    }
+    public class TimeBasedMechanumDriveMovement{
+        public float x;
+        public float y;
+        public float turn;
+        public long time;
+        public long stopTime = 0;
+        public TimeBasedMechanumDriveMovement(float x,float y,float turn, long timeMillis){
+            this.x = x;
+            this.y = y;
+            this.turn = turn;
+            this.time = timeMillis;
         }
     }
 }
